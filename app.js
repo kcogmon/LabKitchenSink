e83569c1f318e71c475f22743dd0e4c1c0ed52ee
Kamesha C <kameshacogmon@gmail.com>
2020-01-15 11:27:18 -0600

    Added files from lab for first time & test

---
+++ app.js
@@ -0,0 +1,32 @@
+// . Name Variables
+let name = "Kamesha";
+// . States Constant
+const STATES = 50
+// . Sume 5 & 4
+let sum = 5 + 4;
+// .Add comments to your code to document its functionality
+// .Write a function called sayHello that displays an alert that says Hello World!Call the say Hello function
+function sayHello() {
+    alert("Hello World!");
+}
+sayHello();
+
+//Write a function called checkAge that takes two arguments: one for a name and one for an age. If the age is less than 21, display an alert that says, "Sorry " + name + ", you aren't old enough to view this page!"
+function checkAge(name, age, gender) {
+    this.name = name
+    this.age = age;
+    this. shape = shape;
+
+    this.
+}
+
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert ('Sorry' '+name+', you aren't old enough to view this page!');
+
+}
+let charles = {
+    name: "Charles",
+    age: 21
+}
